c7dca708ccd1028962db5806d1c1ccb8b804a897
florido_maydelyn <maydelynflorido07@gmail.com>
2025-09-15 22:51:20 +0800

    update todo's'

--- script.js
+++ script.js
@@ -5,13 +5,13 @@
 //        * One of the 5 properties should be of type enum that is created in item no. 2(required).
 //        * One of the 5 properties should be a function with name "print" that does not accept any parameter and returns void.(required)
 //        * The rest of properties are up to you and whether they are required or optional.
-// TODO(Siervo, Jallaine Perpetua), 
-// TODO(Florido, Maydelyn),
 // TODO(Durante, Stephanie),
-// TODO(Riomalos, Zyrrah Feil): 
+// TODO(Florido, Maydelyn),
+// TODO(Riomalos, Zyrrah Feil),
+// TODO(Siervo, Jallaine Perpetua):
 //    create an object out of the Type created in item no. 3. The print method should display all the properties of the object in the format of your choice.
-// TODO(Siervo, Jallaine Perpetua), 
-// TODO(Florido, Maydelyn),
 // TODO(Durante, Stephanie),
-// TODO(Riomalos, Zyrrah Feil): 
+// TODO(Florido, Maydelyn),
+// TODO(Riomalos, Zyrrah Feil),
+// TODO(Siervo, Jallaine Perpetua):
 //    call the print method of he/she created in item no. 4.
