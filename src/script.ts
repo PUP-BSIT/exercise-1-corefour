5b32cb142a8dc31962c5597eab88c7ca2912ab7f
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2025-09-16 00:25:21 +0800

    update: add car3 object and call its print() method

--- src/script.ts
+++ src/script.ts
@@ -17,12 +17,29 @@ type Car = {
 
 // TODO(Durante, Stephanie),
 // TODO(Florido, Maydelyn),
-// TODO(Riomalos, Zyrrah Feil),
+
+const car3: Car = {
+  id: 3,
+  brand: "Honda",
+  model: "Civic",
+  year: 2023,
+  type: CarType.COUPE,
+  print: function () {
+    console.log(
+      `ID: ${this.id},
+       Brand: ${this.brand},
+       Model: ${this.model},
+       Year: ${this.year},
+       Type: ${this.type}`
+    );
+  }
+};
+
 // TODO(Siervo, Jallaine Perpetua):
 //    create an object out of the Type created in item no. 3. The print method should display all the properties of the object in the format of your choice.
 
 // TODO(Durante, Stephanie),
 // TODO(Florido, Maydelyn),
-// TODO(Riomalos, Zyrrah Feil),
+car3.print();
 // TODO(Siervo, Jallaine Perpetua):
 //    call the print method of he/she created in item no. 4.
