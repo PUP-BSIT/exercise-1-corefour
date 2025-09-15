73eba162af848348e7be19cdb02c40378dcca759
florido_maydelyn <maydelynflorido07@gmail.com>
2025-09-15 22:36:52 +0800

    assign todo for every member

---
+++ script.ts
@@ -0,0 +1,22 @@
+// TODO(Durante, Stephanie): 
+//    create an enum of your choice with at least 5 elements of any type.
+
+// TODO(Riomalos, Zyrrah Feil): 
+//    create a Type with at least 5 properties.
+//        * One of the 5 properties should be of type enum that is created in item no. 2(required).
+//        * One of the 5 properties should be a function with name "print" that does not accept any parameter and returns void.(required)
+//        * The rest of properties are up to you and whether they are required or optional.
+
+// TODO(Siervo, Jallaine Perpetua), 
+// TODO(Florido, Maydelyn),
+// TODO(Durante, Stephanie),
+// TODO(Riomalos, Zyrrah Feil): 
+//    create an object out of the Type created in item no. 3. The print method should display all the properties of the object in the format of your choice.
+
+// TODO(Siervo, Jallaine Perpetua), 
+// TODO(Florido, Maydelyn),
+// TODO(Durante, Stephanie),
+// TODO(Riomalos, Zyrrah Feil): 
+//    call the print method of he/she created in item no. 4.
+
+
