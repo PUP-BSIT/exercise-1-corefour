188bb2ea453e25f3c9ec1d1f7a383c2d455ace2c
durante-stephanie <durantestephanie07@gmail.com>
2025-09-16 00:59:35 +0800

    update: add new object and add print method (#6)

--- dist/script.js
+++ dist/script.js
@@ -8,18 +8,39 @@ var CarType;
     CarType["COUPE"] = "Coupe";
     CarType["HATCHBACK"] = "Hatchback";
 })(CarType || (CarType = {}));
-// TODO(Riomalos, Zyrrah Feil):
-//    create a Type with at least 5 properties.
-//        * One of the 5 properties should be of type enum that is created in item no. 2(required).
-//        * One of the 5 properties should be a function with name "print" that does not accept any parameter and returns void.(required)
-//        * The rest of properties are up to you and whether they are required or optional.
-// TODO(Durante, Stephanie),
+const car1 = {
+    id: 1,
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2022,
+    type: CarType.SEDAN,
+    print: function () {
+        console.log(`ID: ${this.id}, 
+      Brand: ${this.brand}, 
+      Model: ${this.model}, 
+      Year: ${this.year}, 
+      Type: ${this.type}`);
+    },
+};
 // TODO(Florido, Maydelyn),
-// TODO(Riomalos, Zyrrah Feil),
+const car3 = {
+    id: 3,
+    brand: "Honda",
+    model: "Civic",
+    year: 2023,
+    type: CarType.COUPE,
+    print: function () {
+        console.log(`ID: ${this.id},
+       Brand: ${this.brand},
+       Model: ${this.model},
+       Year: ${this.year},
+       Type: ${this.type}`);
+    }
+};
 // TODO(Siervo, Jallaine Perpetua):
 //    create an object out of the Type created in item no. 3. The print method should display all the properties of the object in the format of your choice.
-// TODO(Durante, Stephanie),
+car1.print();
 // TODO(Florido, Maydelyn),
-// TODO(Riomalos, Zyrrah Feil),
+car3.print();
 // TODO(Siervo, Jallaine Perpetua):
 //    call the print method of he/she created in item no. 4.
